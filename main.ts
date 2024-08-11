7ed2728c52ede02b3c2dc174c3c3325d3470fb80
omeruysal81 <157880761+omeruysal81@users.noreply.github.com>
2024-08-11 17:01:43 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,50 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . . . #
+        . . . # .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # # .
+        . . . # #
+        . . . # #
+        . . . # #
+        . . # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . # # .
+        . . # # #
+        . . # # #
+        . . # # #
+        . . # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        . . # # #
+        . . . # #
+        . . # # #
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        . # # # #
+        . . # # #
+        . # # # #
+        . # # # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    basic.pause(1000)
+})
